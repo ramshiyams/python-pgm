705813aec7d264e7121ef648b5cae49ba0b4bddb
ramshiyams <ramshiya2020@gmail.com>
2024-10-09 07:08:48 +0100

    Add files via upload

---
+++ week2/largest.py
@@ -0,0 +1,12 @@
+#to find the largest number
+a=int(input("enter the number"))
+b=int(input("enter the second number"))
+c=int(input("enter th last number"))
+if(a>b):
+    lg=a
+elif(b>c):
+    lg=b  
+else:
+    lg=c
+print(lg,"is largest")
+   
---
+++ week2/swaplist.py
@@ -0,0 +1,7 @@
+list=[3,5,6,73,22]
+print("the list before swap",list)
+swap=list[0]
+list[0]=list[-1]
+list[-1]=swap
+print("the list after swap",list)
+
