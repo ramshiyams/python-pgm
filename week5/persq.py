92221a41b7e47afadabd029ff83e716d2b66b342
ramshiyams <ramshiya2020@gmail.com>
2024-10-25 15:25:23 +0530

    Add files via upload

---
+++ week5/persq.py
@@ -0,0 +1,13 @@
+l=[]
+p=[]
+for i in range(1,100):
+    l.append(i)
+print(l)
+for i in l:
+    if i%2==0:
+        p.append(i)
+print("the perfect square")
+for i in p:
+    sq=i*i
+    print(sq)
+        
