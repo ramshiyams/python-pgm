3f396a87ac477871945c8cab6b453a981e3903b7
ramshiyams <ramshiya2020@gmail.com>
2024-12-12 15:39:17 +0530

    Update private_public_oops.py

--- oops/private_public_oops.py
+++ oops/private_public_oops.py
@@ -1,11 +1,11 @@
 class employee:
-    def__init__(self,name,salary):
+    def __init__(self,name,salary):
         #public member
         self.name=name
         #private
         self.__salary=salary
-   def show(self):
-       print("name is",self.name,"salary is",self.__salary)
+    def show(self):
+        print("name is",self.name,"salary is",self.__salary)
 emp=employee("jessa",4000)
 emp.show()
 print(emp.name)
